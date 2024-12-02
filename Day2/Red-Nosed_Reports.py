470159f6d55bd392608b1544738ad2974cccf6e2
Galix <galu0603@gmail.com>
2024-12-02 18:19:04 +0100

    Day2 Part1

---
+++ Day2/Red-Nosed_Reports.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import sys
+
+def diffsAreSmall(numbers: list[int]) -> bool:
+    for i in range(0, len(numbers) - 1):
+        diff = abs(numbers[i] - numbers[i+1])
+        if diff < 1 or diff > 3:
+            return False
+    return True
+
+def isOrdered(numbers: list[int], order_function) -> bool:
+    for i in range(0, len(numbers) - 1):
+        if not order_function(numbers[i], numbers[i+1]):
+            return False
+    return True
+
+def main():
+    input_filepath = 'input.txt'
+    if len(sys.argv) > 1:
+        input_filepath = sys.argv[1]
+
+    with open(input_filepath, mode='r') as input:
+        counter = 0
+        for line in input:
+            numbers = [int(x) for x in line.split()]
+
+            if len(numbers) == 1:
+                counter+=1
+                continue
+
+            if diffsAreSmall(numbers) and (isOrdered(numbers, lambda x,y: x < y) or isOrdered(numbers, lambda x,y: x > y)):
+                counter += 1
+        print(counter)
+
+
+if __name__ == "__main__":
+    main()
