0ea242bf8d86ecf159d32c4629d144f2b3c8379b
Galix <galu0603@gmail.com>
2024-12-10 10:00:02 +0100

    Day9 part 2

--- Day9/Disk-Fragmenter-Part-Two.py
+++ Day9/Disk-Fragmenter-Part-Two.py
@@ -2,33 +2,40 @@
 
 import sys
 
-def defragment(fragment_list):
-    left = 0
+class Fragment:
+    def __init__(self, val, times):
+        self.val = val
+        self.times = times
+
+    def __str__(self):
+        return ''.join([self.val] * self.times) 
+
+def defragment(fragment_list: list[Fragment]):
     right = len(fragment_list) - 1
-    print(fragment_list)
-    while fragment_list[left][1] != '.':
-        left += 1
 
-    while (fragment_list[right][1] == '.' or fragment_list[right][0] > fragment_list[left][0]) and right > left:
-        right -= 1 
+    while right > 0:
+        left = 0
 
-    if right <= left:
-        right = len(fragment_list) - 1
-        left += 1
+        while right > 0 and fragment_list[right].val == '.':
+            right -= 1
 
-    if fragment_list[right][0] == fragment_list[left][0]:
-        fragment_list[left][1] = fragment_list[right][1]
-        fragment_list[right][1] = '.'
-    else:
-        diff = fragment_list[left][0] - fragment_list[right][0]
-        saved_frag = fragment_list[right].copy()
-        fragment_list[right][1] = '.'
-        fragment_list.insert(left, saved_frag)
-        fragment_list[left + 1][0] = diff
-        left += 1
+        while left < right and (fragment_list[left].val != '.' or fragment_list[left].times < fragment_list[right].times):
+            left += 1
 
-    return fragment_list
+        if left >= right:
+            right -= 1
+            continue 
 
+        if fragment_list[left].times == fragment_list[right].times:
+            t = fragment_list[left].val
+            fragment_list[left].val = fragment_list[right].val
+            fragment_list[right].val = t
+        else:
+            diff = fragment_list[left].times - fragment_list[right].times
+            fragment_list.insert(left, Fragment(fragment_list[right].val, fragment_list[right].times))
+            left += 1
+            fragment_list[right+1].val = '.'
+            fragment_list[left].times = diff
 
 def main():
     input_filepath = 'input.txt'
@@ -43,29 +50,27 @@ def main():
     id = 0
     fragment_list = []
     isFree = False
-    pos = 0
     for char in text:
         occurences = int(char)
         if isFree: 
             if occurences > 0:
-                fragment_list.append([occurences,'.'])
+                fragment_list.append(Fragment('.', occurences))
             isFree = False
         else:
             if occurences > 0:
-                fragment_list.append([occurences,id])
+                fragment_list.append(Fragment(str(id), occurences))
             id += 1
             isFree = True
-        pos += occurences
 
-    defragmented = defragment(fragment_list)
-    return 
-    sum = 0
+    defragment(fragment_list)
+
     iter = 0
-    for fragment in defragmented:
-        if fragment[1] != '.':
-            for _ in range(fragment[0]):
-                sum += fragment[1] * iter
-                iter += 1
+    sum = 0
+    for frag in fragment_list:
+        for _ in range(frag.times):
+            if frag.val != '.':
+                sum += iter * int(frag.val)
+            iter += 1
 
     print(sum)
 
