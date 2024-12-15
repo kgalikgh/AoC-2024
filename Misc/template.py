e632219845384aa017e150e4921b42632a8a08e5
Galix <galu0603@gmail.com>
2024-12-15 17:39:28 +0100

    Add python file template

---
+++ Misc/template.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import sys
+
+def main():
+    input_filepath = 'input.txt'
+    if len(sys.argv) > 1:
+        input_filepath = sys.argv[1]
+
+if __name__ == "__main__":
+    main()
